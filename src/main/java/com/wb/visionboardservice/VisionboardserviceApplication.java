3fd435aa9e646a3282f2dd7c961a31406165ddd7
Raul Fredy Whatley (rw407x) <rw407x@att.com>
2019-07-15 18:43:14 -0400

    Base initialized with intellij spring boot initializer

---
+++ src/main/java/com/wb/visionboardservice/VisionboardserviceApplication.java
@@ -0,0 +1,13 @@
+package com.wb.visionboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionboardserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisionboardserviceApplication.class, args);
+    }
+
+}
